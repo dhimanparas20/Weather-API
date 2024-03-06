c0b645507b38437572e3e11d28ae9f755ff0bb7d
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2024-03-07 00:22:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ class HomeF(Resource):
             return getWeatherF(city=query,user_agent=user_agent)
         return({"message":"No city name Provided","syntax1":f"/api/weather/<City Name>","syntax2":f"/api/weather?city=<City Name>","Request Type":"GET","Examples":{"EG1":f"{current_domain}api/weather/Shimla","EG2":f"{current_domain}api/weather?city=Shimla","EG3":f"{current_domain}api/weatherf/171009","EG4":f"{current_domain}api/weatherf?city=171202"}})
         
-api.add_resource(Home, '/','/api/weather','/api/weather/<city>',methods=['GET'])
-api.add_resource(HomeF,'/api/weatherf','/api/weatherf/<city>',methods=['GET'])
+api.add_resource(HomeF, '/','/api/weather','/api/weather/<city>',methods=['GET'])
+api.add_resource(Home,'/api/weatherf','/api/weatherf/<city>',methods=['GET'])
 
 if __name__ == '__main__':
     app.run(debug=False,port=5000,host="0.0.0.0",threaded=True)
